2bdbe0181dc26e1e59d79c55682dfca8646cca69
Garfild63 <garfild63@list.ru>
2021-10-03 17:02:05 +0300

    Beta-version

--- github.bat
+++ github.bat
@@ -2,7 +2,6 @@
 git init
 git add .
 git config --global user.email "garfild63@list.ru"
-git config --global user.name "Garfild63"
 git commit -m "Beta-version"
 git remote add origin https://github.com/Garfild63/parser.git
 git pull origin master
