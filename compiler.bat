8bb9b10f92b642cc5dbf1a77bab130d5eb4637da
Garfild63 <garfild63@list.ru>
2021-10-03 17:34:47 +0300

    Fix

--- compiler.bat
+++ compiler.bat
@@ -1,5 +1,5 @@
 @echo off
 set /p name=
-"C:\Program Files\Java\jdk-15.0.2\bin\javac" Parser.java
+rem "C:\Program Files\Java\jdk-15.0.2\bin\javac" Parser.java
 java Parser %name%
 pause
--- github.bat
+++ github.bat
@@ -1,7 +1,7 @@
 @echo off
 git init
 git add .
-git commit -m "Fix description"
+git commit -m "Fix"
 git remote add origin https://github.com/Garfild63/parser.git
 git pull origin master
 git push origin master
