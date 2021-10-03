60fbe1337059313c5ce7527d9460890f9b4d737e
Garfild63 <garfild63@list.ru>
2021-10-03 17:31:33 +0300

    Fix description

--- github.bat
+++ github.bat
@@ -1,7 +1,7 @@
 @echo off
 git init
 git add .
-git commit -m "Beta-version"
+git commit -m "Fix description"
 git remote add origin https://github.com/Garfild63/parser.git
 git pull origin master
 git push origin master
